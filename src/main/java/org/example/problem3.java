2846626557a1286b728af36f1f5a48e28cbf7269
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-11-16 05:00:24 -1000

    updated problem

--- src/main/java/org/example/problem3.java
+++ src/main/java/org/example/problem3.java
@@ -4,58 +4,27 @@ import com.kennycason.kumo.CollisionMode;
 import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.WordFrequency;
 import com.kennycason.kumo.bg.CircleBackground;
-import com.kennycason.kumo.bg.PixelBoundaryBackground;
-import com.kennycason.kumo.font.scale.LinearFontScalar;
 import com.kennycason.kumo.font.scale.SqrtFontScalar;
 import com.kennycason.kumo.nlp.FrequencyAnalyzer;
 import com.kennycason.kumo.palette.ColorPalette;
 
-import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
-public class problem3 extends JPanel{
-
-    private final WordCloud wordCloud;
+public class problem3{
 
     public static void main(String[] args) throws IOException {
-        final JFrame frame = new JFrame("JPanel WordCloud");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new problem3());
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
-    public problem3() throws IOException {
-        wordCloud = buildWordCloud();
-
-        final JLabel wordCloudLabel = new JLabel(new ImageIcon(wordCloud.getBufferedImage()));
-        add(wordCloudLabel);
-        repaint();
-    }
-
-    private static WordCloud buildWordCloud() throws IOException {
         final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-        frequencyAnalyzer.setWordFrequenciesToReturn(300);
-        frequencyAnalyzer.setMinWordLength(5);
-
-        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(getInputStream("org/example/lyrics.txt"));
-        final Dimension dimension = new Dimension(600, 600);
+        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("lyrics.txt");
+        final Dimension dimension = new Dimension(1000, 1000);
         final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
         wordCloud.setPadding(2);
         wordCloud.setBackground(new CircleBackground(300));
-        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1)));
-        wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+        wordCloud.setFontScalar(new SqrtFontScalar(10, 50));
         wordCloud.build(wordFrequencies);
-
-        return wordCloud;
-    }
-
-    private static InputStream getInputStream(final String path) {
-        return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+        wordCloud.writeToFile("wordcloud.png");
     }
 
 }
