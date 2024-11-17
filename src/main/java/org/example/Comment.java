9788f0aca13774fd39f760d40ee399eeeb120c9e
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-11-16 18:30:49 -1000

    update object attributes

--- src/main/java/org/example/Comment.java
+++ src/main/java/org/example/Comment.java
@@ -2,13 +2,16 @@ package org.example;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
 
 public class Comment {
+    private User creator;
     private String content;
     private Time creationTime;
     private Date date;
 
-    public Comment(String content, Time creationTime, Date date) {
+    public Comment(User creator, String content, Time creationTime, Date date) {
+        this.creator = creator;
         this.content = content;
         this.creationTime = creationTime;
         this.date = date;
--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -2,16 +2,19 @@ package org.example;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
 
 public class Post {
     private String content;
     private Time creationTime;
     private Date date;
+    private List<User> views;
 
-    public Post(String content, Time creationTime, Date date) {
+    public Post(String content, Time creationTime, Date date, List<User> views) {
         this.content = content;
         this.creationTime = creationTime;
         this.date = date;
+        this.views = views;
     }
 
     public String getContent(){
@@ -24,5 +27,9 @@ public class Post {
 
     public Date getDate() {
         return date;
+    }
+
+    public int getAmountOfViews() {
+        return views.size();
     }
 }
