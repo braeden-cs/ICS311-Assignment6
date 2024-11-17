9ca84019c20c37e5eadb0ac3e4b1d0f7bc8200e0
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-11-16 16:42:20 -1000

    changed amount of words in wordcloud

--- src/main/java/org/example/problem3.java
+++ src/main/java/org/example/problem3.java
@@ -16,11 +16,12 @@ public class problem3{
 
     public static void main(String[] args) throws IOException {
         final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        frequencyAnalyzer.setWordFrequenciesToReturn(300);
         final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("lyrics.txt");
         final Dimension dimension = new Dimension(1000, 1000);
         final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
         wordCloud.setPadding(2);
-        wordCloud.setBackground(new CircleBackground(300));
+        wordCloud.setBackground(new CircleBackground(500));
         wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
         wordCloud.setFontScalar(new SqrtFontScalar(10, 100));
         wordCloud.build(wordFrequencies);
